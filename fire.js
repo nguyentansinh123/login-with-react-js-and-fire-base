cf693bcaac46143c73fe26169fb966ebb380ed7b
nguyentansinh123 <66555356+nguyentansinh123@users.noreply.github.com>
2021-08-09 18:28:52 +0700

    Update fire.js

--- fire.js
+++ fire.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC-SWmV4IL5i138_dOuhvXvt3wI4ZyIdJE",
-    authDomain: "auth-development-c6e93.firebaseapp.com",
-    projectId: "auth-development-c6e93",
-    storageBucket: "auth-development-c6e93.appspot.com",
-    messagingSenderId: "807600798276",
-    appId: "1:807600798276:web:ed008d3ef6871c1d11da11"
+    apiKey: .......,
+    authDomain: ...........",
+    projectId: ...........",
+    storageBucket: ...........",
+    messagingSenderId: ...........",
+    appId: ...........",
   };
 
   const fire = firebase.initializeApp(firebaseConfig)
